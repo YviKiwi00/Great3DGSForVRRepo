edf45bed8e117d9d67cd857788c1dcd14716ee4f
Yvette Gyarmati <yvette@yvette-gyarmati.de>
2025-03-07 17:40:17 +0100

    install changes

--- install.py
+++ install.py
@@ -14,17 +14,48 @@ if __name__ == '__main__':
     os.system("conda env create -f environment.yml")
     print("[INFO] Conda environment created.")
 
-    # Install 3D Gaussian Splatting rasterizer
-    print("[INFO] Installing the 3D Gaussian Splatting rasterizer...")
-    os.chdir("server/great3dgsforvr/3dgs-mcmc/")
-    os.system("conda run -n Great3DGSForVR pip install submodules/diff-gaussian-rasterization")
-    print("[INFO] 3D Gaussian Splatting rasterizer installed.")
+    # Install 3D Gaussian Splatting rasterizer and simple-knn for MCMC
+    print("[INFO] Installing the 3D Gaussian Splatting rasterizer for MCMC...")
+    os.chdir("server/great3dgsforvr/3dgs-mcmc/submodules/")
+    os.system("git clone --recursive https://github.com/ashawkey/diff-gaussian-rasterization")
+    os.system("conda run -n Great3DGSForVR pip install ./diff-gaussian-rasterization")
+    print("[INFO] 3D Gaussian Splatting rasterizer for MCMC installed.")
 
-    # Install simple-knn
-    print("[INFO] Installing simple-knn...")
-    os.system("conda run -n Great3DGSForVR pip install submodules/simple-knn/")
-    print("[INFO] simple-knn installed.")
-    os.chdir("../../../")
+    print("[INFO] Installing simple-knn for MCMC...")
+    os.system("conda run -n Great3DGSForVR pip install ./simple-knn")
+    print("[INFO] simple-knn for MCMC installed.")
+    os.chdir("../../../../")
+
+    # Install 3D Gaussian Splatting rasterizer, simple-knn, SAM and GroundingDINO for SAGD
+    print("[INFO] Installing the 3D Gaussian Splatting rasterizer for SAGD...")
+    os.chdir("server/great3dgsforvr/SAGS/gaussiansplatting/submodules/")
+    os.system("git clone --recursive https://github.com/ashawkey/diff-gaussian-rasterization")
+    os.system("conda run -n Great3DGSForVR pip install ./diff-gaussian-rasterization")
+    print("[INFO] 3D Gaussian Splatting rasterizer for SAGD installed.")
+
+    print("[INFO] Installing simple-knn for SAGD...")
+    os.system("conda run -n Great3DGSForVR pip install ./simple-knn")
+    print("[INFO] simple-knn for SAGD installed.")
+    os.chdir("../")
+
+    print("[INFO] Installing SAM for SAGD...")
+    os.makedirs("dependencies", exist_ok=True)
+    os.chdir("dependencies/")
+    os.system("wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth")
+    os.system("git clone git@github.com:facebookresearch/segment-anything.git")
+    os.chdir("segment-anything/")
+    os.system("conda run -n Great3DGSForVR pip install -e .")
+    print("[INFO] SAM for SAGD installed.")
+
+    print("[INFO] Installing GroundingDINO for SAGD...")
+    os.system("git clone https://github.com/IDEA-Research/GroundingDINO.git")
+    os.chdir("GroundingDINO/")
+    os.system("conda run -n Great3DGSForVR pip install -e .")
+    os.makedirs("weights/")
+    os.chdir("weights/")
+    os.system("wget https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth")
+    print("[INFO] GroundingDino for SAGD installed.")
+    os.chdir("../../../../../../../../../")
 
     # Install Nvdiffrast
     if args.no_nvdiffrast:
