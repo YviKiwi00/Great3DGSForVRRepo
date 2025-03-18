9fc2c1cd59fa2df19ed94f446c897c1fec13b726
Yvette Gyarmati <yvette@yvette-gyarmati.de>
2025-03-18 21:09:56 +0100

    jobs table polling

--- client/static/js/jobs.js
+++ client/static/js/jobs.js
@@ -1,16 +1,27 @@
 document.addEventListener("DOMContentLoaded", async () => {
     const table = document.getElementById("jobTable").querySelector("tbody");
-    table.innerHTML = "";
 
-    const response = await fetch('/jobs');
-    const jobs = await response.json();
-
-    jobs.forEach(job => {
-        const row = document.createElement('tr');
-        row.innerHTML = `<td>${job.id}</td><td>${job.project_name}</td><td>${job.status}</td>`;
-        row.onclick = () => {
-            window.location.href = `job.html?id=${job.id}`;
-        };
-        table.appendChild(row);
-    });
+    pollJobTable(table);
 });
+
+function pollJobTable(table) {
+    async function fetchAndUpdateTable() {
+        table.innerHTML = "";
+
+        const response = await fetch('/jobs');
+        const jobs = await response.json();
+
+        jobs.forEach(job => {
+            const row = document.createElement('tr');
+            row.innerHTML = `<td>${job.id}</td><td>${job.project_name}</td><td>${job.status}</td>`;
+            row.onclick = () => {
+                window.location.href = `job.html?id=${job.id}`;
+            };
+            table.appendChild(row);
+        });
+
+        setTimeout(fetchAndUpdateTable, 5000);
+    }
+
+    fetchAndUpdateTable();
+}
