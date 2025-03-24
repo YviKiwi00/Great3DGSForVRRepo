69da78e6c7475af2d1ed6e4a73c1f13e6052bdc5
Yvette Gyarmati <yvette@yvette-gyarmati.de>
2025-03-24 15:15:16 +0100

    server conf printing

--- server/server.py
+++ server/server.py
@@ -8,6 +8,7 @@ from controllers.colmap_controller import router as colmap_router
 from controllers.mcmc_controller import router as mcmc_router
 from controllers.segmentation_controller import router as segmentation_router
 from controllers.frosting_controller import router as frosting_router
+from utils.jobs_utils import get_exp_values
 
 from jobs_queue import start_worker, shutdown_handler
 
@@ -24,5 +25,11 @@ app.include_router(segmentation_router)
 app.include_router(frosting_router)
 
 if __name__ == "__main__":
+    mcmc_iterations, mcmc_cap_max, frosting_gauss = get_exp_values()
+    print("Starting server with configuration:")
+    print(f"MCMC_IT = {mcmc_iterations}")
+    print(f"MCMC_CAP_MAX = {mcmc_cap_max}")
+    print(f"FROSTING_GAUSS = {frosting_gauss}")
+
     start_worker()
     uvicorn.run(app, host="0.0.0.0", port=5000)
